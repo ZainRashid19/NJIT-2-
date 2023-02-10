eed93e15610682234ee0bf11f463aa6a43163c49
ZainRashid19 <92735725+ZainRashid19@users.noreply.github.com>
2023-02-10 11:32:49 -0500

    Add files via upload

--- gallery.js
+++ gallery.js
@@ -64,6 +64,38 @@ if(mCurrentIndex<0){
 
 }
 
+function swapPhoto() {
+	//Add code here to access the #slideShow element.
+	//Access the img element and replace its source
+	//with a new image from your images array which is loaded 
+	//from the JSON string
+
+if(mCurrentIndex>=mImages.length){
+	mCurrentIndex=0;
+
+}
+
+if(mCurrentIndex<0){
+	mCurrentIndex=mImages.length-1;
+
+} 
+
+
+	document.getElementById('photo').src = mImages[mCurrentIndex].img;
+	var loc= document.getElementsByClassName('location');
+	loc[0].innerHTML= "Location: " + mImages[mCurrentIndex].location;
+
+	var des = document.getElementsByClassName('description');
+	des[0].innerHTML= "description: " + mImages[mCurrentIndex].description;
+
+	var dt = document.getElementsByClassName('date');
+	dt[0].innerHTML= "Date: " + mImages[mCurrentIndex].date;
+
+	mLastFrameTime=0
+	mCurrentIndex +=1
+
+}
+
 // Counter for the mImages array
 var mCurrentIndex = 0;
 
