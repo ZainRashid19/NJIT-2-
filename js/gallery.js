f7a1323478b84a4469cc6605b23b6233f8969c10
ZainRashid19 <92735725+ZainRashid19@users.noreply.github.com>
2023-02-27 12:12:11 -0500

    Kilgore saves the day
    Mr. Kilgore realized I didn't check my files before I submitted them and then he fixed my issue so I didn't fail.   He's the best.

--- js/gallery.js
+++ js/gallery.js
@@ -37,7 +37,65 @@ function swapPhoto() {
 	//Access the img element and replace its source
 	//with a new image from your images array which is loaded 
 	//from the JSON string
-	console.log('swap photo');
+
+if(mCurrentIndex>=mImages.length){
+	mCurrentIndex=0;
+
+}
+
+if(mCurrentIndex<0){
+	mCurrentIndex=mImages.length-1;
+
+} 
+
+
+	document.getElementById('photo').src = mImages[mCurrentIndex].img;
+	var loc= document.getElementsByClassName('location');
+	loc[0].innerHTML= "Location: " + mImages[mCurrentIndex].location;
+
+	var des = document.getElementsByClassName('description');
+	des[0].innerHTML= "description: " + mImages[mCurrentIndex].description;
+
+	var dt = document.getElementsByClassName('date');
+	dt[0].innerHTML= "Date: " + mImages[mCurrentIndex].date;
+
+	mLastFrameTime=0
+	mCurrentIndex +=1
+
+}
+
+
+
+function previousclick() {
+	//Add code here to access the #slideShow element.
+	//Access the img element and replace its source
+	//with a new image from your images array which is loaded 
+	//from the JSON string
+
+if(mCurrentIndex>=mImages.length){
+	mCurrentIndex=0;
+
+}
+
+if(mCurrentIndex<0){
+	mCurrentIndex=mImages.length-1;
+
+} 
+
+
+	document.getElementById('photo').src = mImages[mCurrentIndex].img;
+	var loc= document.getElementsByClassName('location');
+	loc[0].innerHTML= "Location: " + mImages[mCurrentIndex].location;
+
+	var des = document.getElementsByClassName('description');
+	des[0].innerHTML= "description: " + mImages[mCurrentIndex].description;
+
+	var dt = document.getElementsByClassName('date');
+	dt[0].innerHTML= "Date: " + mImages[mCurrentIndex].date;
+
+	mLastFrameTime=0
+	mCurrentIndex -=1
+
 }
 
 // Counter for the mImages array
@@ -50,12 +108,40 @@ var mRequest = new XMLHttpRequest();
 var mImages = [];
 
 // Holds the retrived JSON information
-var mJson;
+var mJson 
 
 // URL for the JSON to load by default
 // Some options for you are: images.json, images.short.json; you will need to create your own extra.json later
-var mUrl = 'insert_url_here_to_image_json';
+var mUrl = 'new.json';
 
+//Part 2 Slideshow 1 and 2 
+
+function fetchJSON(){
+	mRequest.onreadystatechange = function(){
+		console.log("on ready state change");
+		if(this.readyState == 4 && this.status==200){
+	mJson = JSON.parse(mRequest.responseText);
+	iterateJSON(mJson);
+		}
+	}
+	mRequest.open('GET',mUrl,true);
+	mRequest.send();
+}
+
+// function made to iterate and then give the variables values for their images
+function iterateJSON(mJson){
+    for (let x = 0; x < mJson.images.length; x++) {
+		mImages[x] = new GalleryImage();
+		
+		mImages[x].location = mJson.images[x].imgLocation;
+		mImages[x].description = mJson.images[x].description;
+		mImages[x].date = mJson.images[x].date;
+		mImages[x].img = mJson.images[x].imgPath;
+
+
+	}
+
+}
 
 //You can optionally use the following function as your event callback for loading the source of Images from your json data (for HTMLImageObject).
 //@param A GalleryImage object. Use this method for an event handler for loading a gallery Image object (optional).
@@ -66,10 +152,28 @@ function makeGalleryImageOnloadCallback(galleryImage) {
 	}
 }
 
+//Part 5 
 $(document).ready( function() {
-	
 	// This initially hides the photos' metadata information
-	$('.details').eq(0).hide();
+	const urlParams = new URLSearchParams(window.location.search);
+
+for (const [key, value] of urlParams) {
+    console.log(`${key}:${value}`);
+	mUrl = value
+} 
+	if(mUrl==undefined){
+		mUrl = "https://api.npoint.io/a2a427005054ea24b4ae";
+	}
+	
+	
+	
+	
+	
+	
+	
+	fetchJSON();
+
+	//$('.details').eq(0).hide();
 	
 });
 
@@ -79,10 +183,33 @@ window.addEventListener('load', function() {
 
 }, false);
 
+
+//Part 1 defining the gallery image for the functions 
 function GalleryImage() {
+	var location ;
+	var description ;
+	var date ;
+	var img ;  
+
+
+
+
 	//implement me as an object to hold the following data about an image:
 	//1. location where photo was taken
 	//2. description of photo
 	//3. the date when the photo was taken
 	//4. either a String (src URL) or an an HTMLImageObject (bitmap of the photo. https://developer.mozilla.org/en-US/docs/Web/API/HTMLImageElement)
+}
+
+function showDetails() {
+
+	if ($( ".moreIndicator" ).hasClass( "rot90" )){
+		$( ".moreIndicator" ).addClass( "rot270" );
+	$( ".moreIndicator" ).removeClass( "rot90" )
+	}
+	else {
+		$( ".moreIndicator" ).removeClass( "rot270" );
+	$( ".moreIndicator" ).addClass( "rot90" )
+	}
+	$( ".details" ).slideToggle( "fast" )
 }
